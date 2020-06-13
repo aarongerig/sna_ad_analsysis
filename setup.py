b064522c8769e06bc12180f0720d2be8754e644a
Aaron Gerig <a.gerig@w-vision.ch>
2020-06-13 15:50:57 +0200

    chore: project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Insertion behavior and potential of the Woche-Pass',
+    author='Aaron Gerig',
+    license='BSD-3',
+)
